8b8bb5b15f17f607589aba1fdd41a693bd42b64c
Muhammed Nihad <muhammednihad999@gmail.com>
2023-12-01 23:08:55 +0530

    feat: register Category and ProductItem models in admin ✨
    Registers Category model and ProductItem model with TabularInline inorder add product image while adding products.

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,3 +1,69 @@
-from django.contrib import admin
+from django.contrib.admin import TabularInline, register
 
-# Register your models here.
+from src.common.admin import BaseAdmin, SaveModelAdmin
+
+from .models import Category, ProductImage, ProductItem
+
+
+@register(Category)
+class CategoryAdmin(BaseAdmin, SaveModelAdmin):  # type: ignore
+    """
+    Admin configuration for Category Model
+    """
+
+    fieldsets = (
+        (
+            None,
+            {"fields": ("name", "description", "icon")},
+        ),
+    )
+    list_display = ["name", "created_by", "updated_by", "created", "modified"]
+    list_filter = ["created", "modified"]
+    search_fields = ["name"]
+
+
+class ProductImageInline(TabularInline):  # type: ignore
+    """
+    Inline configuration for ProductImage in the admin interface.
+    """
+
+    model = ProductImage
+    exclude = ["updated_by"]
+    view_on_site = False
+
+
+@register(ProductItem)
+class ProductItemAdmin(BaseAdmin, SaveModelAdmin):  # type: ignore
+    """
+    Admin configuration for ProductItem Model
+    """
+
+    inlines = [ProductImageInline]
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "name",
+                    "category",
+                    "description",
+                    "stock",
+                    "og_price",
+                    "price",
+                    "is_published",
+                    "is_new_arrival",
+                    "is_featured",
+                ),
+            },
+        ),
+    )
+    list_display = [
+        "name",
+        "category",
+        "created_by",
+        "updated_by",
+        "created",
+        "modified",
+    ]
+    list_filter = ["created", "modified"]
+    search_fields = ["name"]
