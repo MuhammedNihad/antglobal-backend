12e2e876e0c76ec0849c9223e34b750a4b520618
Muhammed Nihad <muhammednihad999@gmail.com>
2023-12-03 23:33:08 +0530

    chore: integrate `django-debug-toolbar` package
    Adds django-debug-toolbar package to enhance development experience

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,15 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    # Appends Django Debug Toolbar to urlpatterns in developement environment
+    if "debug_toolbar" in settings.INSTALLED_APPS:
+        import debug_toolbar
+
+        urlpatterns += (path("__debug__/", include(debug_toolbar.urls)),)
